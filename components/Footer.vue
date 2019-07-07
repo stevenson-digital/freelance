49c3e9a0cf6d11525d2e25026a67c6cc96fc6ae4
Ollie Stevenson <ollie@stevenson.digital>
2019-07-07 17:39:42 +0100

    [add] desktop footer HTML and CSS

---
+++ components/Footer.vue
@@ -0,0 +1,63 @@
+<template>
+  <footer class="[ c-Footer ] [ pt-110-50 pb-110-50 ]">
+    <div class="container">
+      <div class="row">
+        <div class="columns">
+          <a class="[ c-Footer__link ] [ u-color-white f-heading-98-46 ]" :href="'tel:+447760668039'">
+            Tel:<span class="f-heading-98-46--light">+447760668039</span>
+          </a>
+          <a class="[ c-Footer__link ] [ u-color-white f-heading-98-46 ]" :href="'mailto:ollie@stevenson.digital'">
+            Email:<span class="f-heading-98-46--light">ollie@stevenson.digital</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+@import './assets/scss/main.scss';
+
+/* ===========================
+  Block
+=========================== */
+.c-Footer {
+  background-color: $c-black;
+}
+
+/* ===========================
+  Elements
+=========================== */
+.c-Footer__link {
+  display: table;
+  margin-bottom: 15px;
+  text-decoration: none;
+
+  span {
+    position: relative;
+    display: inline-block;
+    margin-left: 20px;
+
+    &:after {
+      content: '';
+      position: absolute;
+      bottom: -4px;
+      left: 0;
+      width: 0;
+      height: 2px;
+      background-color: $c-white;
+      transition: width 425ms $t-cubic-bez;
+    }
+  }
+
+  &:hover span::after {
+    width: 100%;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,15 +2,18 @@
   <div>
     <NavBar />
     <nuxt />
+    <Footer />
   </div>
 </template>
 
 <script>
 import NavBar from '~/components/NavBar'
+import Footer from '~/components/Footer'
 
 export default {
   components: {
-    NavBar
+    NavBar,
+    Footer
   }
 }
 </script>
