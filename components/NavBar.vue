5718a91af693a442dcaa22ffe7befa2bb3983189
Ollie Stevenson <ollie@stevenson.digital>
2019-07-07 17:03:22 +0100

    [add] scrolled navbar state

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="c-NavBar">
+  <div :class="['c-NavBar', {'scrolled': scrolled}]">
     <img class="c-NavBar__logo" src="~/assets/img/Logo_Black.png" alt="Ollie Stevenson freelance web developer and designer logo">
     <a class="c-NavBar__email" href="mailto:ollie@stevenson.digital">ollie@stevenson.digital</a>
   </div>
@@ -7,7 +7,29 @@
 
 <script>
 export default {
-
+  data() {
+    return {
+      scrolled: false
+    }
+  },
+  mounted() {
+    this.toggleScrolledState()
+  },
+  beforeMount() {
+    window.addEventListener('scroll', this.toggleScrolledState)
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.toggleScrolledState)
+  },
+  methods: {
+    toggleScrolledState() {
+      if (window.scrollY > 0) {
+        this.scrolled = true
+      } else {
+        this.scrolled = false
+      }
+    }
+  }
 }
 </script>
 
@@ -24,9 +46,23 @@ export default {
   justify-content: space-between;
   align-items: center;
   position: fixed;
-  top: $container-padding;
+  top: 0;
   left: 0;
   height: $h-navbar;
+  z-index: $z-navbar;
+  background-color: $c-white;
+  border: 1px solid transparent;
+  transform: translateY($container-padding);
+  transition: transform $t-standard, border-color $t-standard;
+
+  &.scrolled {
+    transform: translateY(0px);
+    border-color: $c-light-grey;
+
+    .c-NavBar__logo {
+      height: 60%;
+    }
+  }
 }
 
 /* ===========================
@@ -35,6 +71,7 @@ export default {
 .c-NavBar__logo {
   height: 100%;
   margin-left: $container-padding;
+  transition: height $t-standard;
 }
 
 .c-NavBar__email {
